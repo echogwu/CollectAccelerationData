aa03893833a9471466d169e824f256fdc0b6cd2b
Echo Wu <echo@aerserv.com>
2018-01-16 04:52:42 -0800

    Use deviceMotion to get gravitational and user acceleration data.

--- CollectAccelerationData/ViewController.swift
+++ CollectAccelerationData/ViewController.swift
@@ -7,11 +7,42 @@
 //
 
 import UIKit
+import CoreMotion
+import simd
 
 class ViewController: UIViewController {
 
+    let motion: CMMotionManager = CMMotionManager()
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = paths[0]
+        return documentsDirectory
+    }
+    
+    func startAccelerator(){
+        if motion.isAccelerometerAvailable{
+            motion.deviceMotionUpdateInterval = 1.0 / 60.0   // 60HZ
+            motion.startDeviceMotionUpdates(using: .xArbitraryZVertical, to: .main, withHandler: { (deviceMotion, error) in
+                guard let deviceMotion = deviceMotion else { return }
+                self.collectData(from: deviceMotion)
+                
+            })
+        }
+    }
+    
+    func collectData(from deviceMotion: CMDeviceMotion){
+        let gravity = [deviceMotion.gravity.x, deviceMotion.gravity.y, deviceMotion.gravity.z]
+        let userAcceleration = [deviceMotion.userAcceleration.x, deviceMotion.userAcceleration.y, deviceMotion.userAcceleration.z]
+        print("ducument directory: \(getDocumentsDirectory())")
+        print("gravity: \(gravity)")
+        print("userAcceleration: \(userAcceleration)")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        startAccelerator()
+        print(getDocumentsDirectory())
         // Do any additional setup after loading the view, typically from a nib.
     }
 
