022ad8a8cfa0228b950e9bb5e75fb8c488a39fba
Echo Wu <echo@aerserv.com>
2018-01-16 04:09:37 -0800

    Initial Commit

---
+++ CollectAccelerationData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollectAccelerationData
+//
+//  Created by  Echo Wu on 1/16/18.
+//  Copyright © 2018  Echo Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
